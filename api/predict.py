6c1b4c6ddb55976d11d6d3d7a0fc45c436e108a0
mrfiiqane <maxamedfx064@gmail.com>
2025-09-30 12:04:24 +0300

    1

--- api/predict.py
+++ api/predict.py
@@ -2,6 +2,18 @@ import json
 import pandas as pd
 import joblib
 from Utilities import prepare_features_from_raw
+import joblib
+import urllib.request
+import os
+
+MODEL_URL = "https://github.com/mrfiiqane/House_Price_Prediction/raw/main/modeles/rf_model.joblib"
+MODEL_PATH = "/tmp/rf_model.joblib"
+
+if not os.path.exists(MODEL_PATH):
+    urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
+
+rf_model = joblib.load(MODEL_PATH)
+
 
 # Load models once
 MODELS = {
